fbd0175b4116cd3d2c58baf0df71ab4c66633bf4
Saggre <evo@live.fi>
2017-10-10 12:46:24 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Blank-Bootstrap
+# talvipaivat
---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Joinment
+ *
+ * @author      Sakri Koskimies
+ * @copyright (c) 2017 Joinment Oy, Sakri Koskimies
+ *
+ * @wordpress-plugin
+ * Plugin Name: Joinment
+ * Plugin URI:  https://joinment.com/theme
+ * Description: Description of the plugin.
+ * Version:     0.1.0
+ * Author:      Joinment Oy
+ * Text Domain: joinment
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="fi">
+<head>
+    <title></title>
+
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+
+    <link rel="stylesheet" type="text/css" href="/style/bootstrap-grid.min.css"/>
+    <link rel="stylesheet" type="text/css" href="/style/bootstrap.min.css"/>
+    <link rel="stylesheet" type="text/css" href="/style.min.css"/>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,200,400&amp;subset=latin-ext" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/8744b3dda7.css"/>
+
+    <script src="/js/jquery.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/scrollreveal.min.js"></script>
+    <script src="/js/main.min.js"></script>
+
+
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"
+            integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+            crossorigin="anonymous"></script>
+</head>
+<body>
+
+</body>
+</html>
